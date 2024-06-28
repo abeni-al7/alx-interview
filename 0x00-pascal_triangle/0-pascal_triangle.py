81ae979f7b548ba22b947a3b18c73d879d1ae206
abeni-al7 <abenezeralebachew3@gmail.com>
2024-06-28 03:19:11 +0300

    Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    pascal = [[1]]
+    for i in range(n - 1):
+        temp = [0] + pascal[-1] + [0]
+        pascal_row = []
+        for j in range(len(pascal[-1]) + 1):
+            pascal_row.append(temp[j] + temp[j+1])
+        pascal.append(pascal_row)
+    return pascal
