7d6b2791e01fd071011e468f23a73deecf9314d0
abeni-al7 <abenezeralebachew3@gmail.com>
2024-07-19 04:18:10 +0300

    Solve log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,52 @@
 #!/usr/bin/python3
 '''log parsing solution'''
 import sys
-import signal
-
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+import re
 
 
-def print_stats():
-    '''prints the stats'''
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+def initialize_log():
+    '''initializes log'''
+    status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    log = {"file_size": 0,
+           "code_list": {str(code): 0 for code in status_codes}}
+    return log
 
 
-def signal_handler(sig, frame):
-    '''handles signal'''
-    print_stats()
-    sys.exit(0)
+def parse_line(line, regex, log):
+    '''parses each line'''
+    match = regex.fullmatch(line)
+
+    if match:
+        stat_code, file_size = match.group(1, 2)
+
+        log["file_size"] += int(file_size)
+
+        if stat_code.isdecimal():
+            log["code_list"][stat_code] += 1
+    return log
 
 
-signal.signal(signal.SIGINT, signal_handler)
+def print_codes(log):
+    '''prints stats'''
+    print("File size: {}".format(log['file_size']))
+    sorted_code_list = sorted(log["code_list"])
+    for code in sorted_code_list:
+        if log["code_list"][code]:
+            print(f"{code}: {log['code_list'][code]}")
 
-try:
+
+def main():
+    '''Entry point'''
+    regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
+    log = initialize_log()
+    line_count = 0
     for line in sys.stdin:
-        parts = line.split()
-        if len(parts) != 7:
-            continue
-        ip, dash, date, request, protocol, status, size = parts
-        if request != '"GET' or protocol != 'HTTP/1.1"' or not status.isdigit() or not size.isdigit():
-            continue
-        status = int(status)
-        size = int(size)
-        total_size += size
-        if status in status_codes:
-            status_codes[status] += 1
+        line = line.strip()
         line_count += 1
+        parsed_log = parse_line(line, regex, log)
         if line_count % 10 == 0:
-            print_stats()
-except KeyboardInterrupt:
-    print_stats()
-    sys.exit(0)
+            print_codes(parsed_log)
+
+
+if __name__ == '__main__':
+    main()
