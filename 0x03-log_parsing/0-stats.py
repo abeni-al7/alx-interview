be7477fc6041ed09d765d0a1440ee0ded799a9d5
abeni-al7 <abenezeralebachew3@gmail.com>
2024-07-19 03:26:55 +0300

    Solve log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-'''Log parsing python solution'''
 import sys
 import signal
 
-
 total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
-
 
 def print_stats():
     print(f"File size: {total_size}")
@@ -16,30 +12,25 @@ def print_stats():
         if status_codes[code] > 0:
             print(f"{code}: {status_codes[code]}")
 
-
 def signal_handler(sig, frame):
     print_stats()
     sys.exit(0)
-
 
 signal.signal(signal.SIGINT, signal_handler)
 
 try:
     for line in sys.stdin:
         parts = line.split()
-        if len(parts) != 8:
+        if len(parts) != 7:
             continue
-        ip, dash, date, method, path, protocol, status, size = parts
-        if method != '"GET' or protocol != 'HTTP/1.1"':
+        ip, dash, date, request, protocol, status, size = parts
+        if request != '"GET' or protocol != 'HTTP/1.1"' or not status.isdigit() or not size.isdigit():
             continue
-        if not status.isdigit() or not size.isdigit():
-            continue
-        status, size = int(status), int(size)
+        status = int(status)
+        size = int(size)
+        total_size += size
         if status in status_codes:
             status_codes[status] += 1
-        else:
-            continue
-        total_size += size
         line_count += 1
         if line_count % 10 == 0:
             print_stats()
